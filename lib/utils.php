fe39c5d39b40051ea2203e9ee565b55fa60b0175
Nur Rachmat <rachmat.nur91@gmail.com>
2024-04-26 08:40:20 +0700

    update comment

--- lib/utils.php
+++ lib/utils.php
@@ -1,10 +1,22 @@
 <?php 
-//fungsi untuk cek status, merubah status menjadi Ya atau Tidak 
+/**
+     * Fungsi untuk cek status, merubah status menjadi Ya atau Tidak
+     *
+     * @param  int  $status
+     * @return string
+	 * 		String dengan nilai Ya atau Tidak
+*/
 function cekStatus($status){
     return ($status == "1") ? "Ya" : "Tidak";
 }
 
-//fungsi untuk konversi tanggal
+/**
+     * Fungsi untuk mengkonversi tanggal menjadi format indo
+     *
+     * @param  string  $date
+     * @return string
+	 * 		String dengan format Tgl Bulan Tahun, cth : 17 Agustus 1945
+*/
 function konversiTanggal($date){
     $tanggal = date("Y-m-d", strtotime($date));
     $bulan = array (
@@ -25,7 +37,12 @@ function konversiTanggal($date){
 	return $pecahkan[2] . ' ' . $bulan[ (int)$pecahkan[1] ] . ' ' . $pecahkan[0];
 }
 
-//fungsi untuk menampilkan nominal dalam rupiah
+/**
+     * Fungsi untuk menampilkan nominal dalam rupiah
+     *
+     * @param  int  $nominal
+     * @return string
+*/
 function rp($nominal){
     return "Rp " . number_format($nominal, 2, ",", ".");
 }
